647099067c1d79976393b3a459f9e15d82c30017
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-10-20 19:49:38 -1000

    added media queries for larger screens

--- src/Components/ResultCards.jsx
+++ src/Components/ResultCards.jsx
@@ -3,10 +3,10 @@ import '../assets/css/ResultCards.css'
 
 export function ResultCards(props){
     return (
-        <div className='result-cards'id={props.id} key={props.key} onClick={props.handleClick}>
-            <img className="poster-image" src={`https://image.tmdb.org/t/p/w200/${props.poster}`} alt="Poster Image" />
-            <h3>{props.title}</h3>
-            <p>{props.release}</p>
+        <div className='result-cards'id={props.id} key={props.key}>
+            <img className="poster-image" onClick={props.handleClick} src={`https://image.tmdb.org/t/p/w200/${props.poster}`} alt="Poster Image" />
+            <h3 className='info'>{props.title}</h3>
+            <h3 className='info'>{props.release}</h3>
         </div>
     )
 }
