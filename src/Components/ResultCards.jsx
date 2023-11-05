f6be295cdeceef9ab70c913653721d27302f2891
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-11-04 18:53:30 -1000

    adjust css for when low amount of movies are populated

--- src/Components/ResultCards.jsx
+++ src/Components/ResultCards.jsx
@@ -1,10 +1,10 @@
 import '../assets/css/ResultCards.css'
-
+import error from '../assets/images/no-image.svg'
 
 export function ResultCards(props){
     return (
         <div className='result-cards'id={props.id} key={props.key}>
-            <img className="poster-image" onClick={props.handleClick} src={`https://image.tmdb.org/t/p/w200/${props.poster}`} alt="Poster Image" />
+            <img className="poster-image" onClick={props.handleClick} src={`https://image.tmdb.org/t/p/w200/${props.poster}`} alt="Poster Image"/>
             <h3 className='info'>{props.title}</h3>
             <h3 className='info'>{props.release}</h3>
         </div>
